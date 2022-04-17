92bfc87083707caf006fa19f884beb1fb272c74c
James Nicholson <jaemsnicholson196@gmail.com>
2022-04-17 21:38:16 +0100

    added Zuul gateway

---
+++ zuul-gateway/src/main/java/com/assignment/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.assignment.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulGatewayApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(ZuulGatewayApplication.class);
+    }
+
+}
